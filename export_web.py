c7d197cdbaa0154f8436138f25ed113293b732fd
Jartible <jartibledev@proton.me>
2026-04-21 17:00:27 +0200

    Correct by AI

--- export_web.py
+++ export_web.py
@@ -22,114 +22,89 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
-
 import gi
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp
 gi.require_version('GimpUi', '3.0')
 from gi.repository import GimpUi
-
 from gi.repository import GLib
 
 class ExportWeb (Gimp.PlugIn):
     def do_query_procedures(self):
-        return [ "-plugin-export-to-web" ]
+        return [ "plugin-export-to-web" ]
 
     def do_set_i18n (self, name):
         return False
 
     def do_create_procedure(self, name):
+        # Using Gimp.RunMode.NONINTERACTIVE if you want it to run without popups
         procedure = Gimp.ImageProcedure.new(self, name,
                                             Gimp.PDBProcType.PLUGIN,
                                             self.run, None)
-
         procedure.set_image_types("*")
-
         procedure.set_menu_label("Export to web")
-        procedure.add_menu_path('<Image>/Export to web')
+        procedure.add_menu_path('<Image>/File/Export') # Put it in File > Export
 
-        procedure.set_documentation("This plugin export files which size is less than 2 MB",
+        procedure.set_documentation("Exports file optimized for web (< 2MB target)",
                                     "Plugin export to web",
                                     name)
-        procedure.set_attribution("Maya López", "Sergio", "2026")
-
+        procedure.set_attribution("Maya López & Sergio", "Sergio", "2026")
         return procedure
 
-    def run(self, procedure, run_mode, image, drawable, config, run_data, filename):
-        #name_image = image.name
+    def run(self, procedure, run_mode, image, drawables, config, run_data):
+        # GIMP 3.0 passes 'drawables' as a list/array
         
-        procedure = Gimp.get_pdb().lookup_procedure('gimp-image-flatten');
-        config = procedure.create_config();
-        config.set_property('image', image);
-        result = procedure.run(config);
-        success = result.index(0);
-        layer = result.index(1)
+        # 1. Flatten Image
+        flatten_proc = Gimp.get_pdb().lookup_procedure('gimp-image-flatten')
+        flatten_conf = flatten_proc.create_config()
+        flatten_conf.set_property('image', image)
+        flatten_proc.run(flatten_conf)
         
-        width = image.width
-        height = image.height
+        width = image.get_width()
+        height = image.get_height()
 
-        if height < 3000:
+        # 2. Scale if too large
+        if height > 3000:
             new_height = 3000
-            new_width = (width * new_height)/ height
+            new_width = int((width * new_height) / height)
 
-            procedure = Gimp.get_pdb().lookup_procedure('gimp-image-resize'); 
-            config = procedure.create_config(); 
-            config.set_property('image', image); 
-            config.set_property('new-width', new_width); 
-            config.set_property('new-height', new_height); 
-            config.set_property('offx', 0); 
-            config.set_property('offy', 0); 
-            result = procedure.run(config); 
-            success = result.index(0)
+            scale_proc = Gimp.get_pdb().lookup_procedure('gimp-image-scale')
+            scale_conf = scale_proc.create_config()
+            scale_conf.set_property('image', image)
+            scale_conf.set_property('new-width', new_width)
+            scale_conf.set_property('new-height', new_height)
+            scale_proc.run(scale_conf)
 
-            procedure = Gimp.get_pdb().lookup_procedure('gimp-image-set-resolution'); 
-            config = procedure.create_config(); 
-            config.set_property('image', image); 
-            config.set_property('xresolution', 72); 
-            config.set_property('yresolution', 72);
-            result = procedure.run(config);
-            success = result.index(0)
+        # 3. Set Resolution
+        res_proc = Gimp.get_pdb().lookup_procedure('gimp-image-set-resolution')
+        res_conf = res_proc.create_config()
+        res_conf.set_property('image', image)
+        res_conf.set_property('xresolution', 72.0)
+        res_conf.set_property('yresolution', 72.0)
+        res_proc.run(res_conf)
 
-        else :
-            print("The file is to small")
+        # 4. Prepare Export File (Convert string path to GFile)
+        # Note: You need a destination. For this example, I'll save to Desktop/web_export.jpg
+        export_path = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP) + "/web_export.jpg"
+        save_file = Gio.File.new_for_path(export_path)
+
+        # 5. JPEG Export
+        export_proc = Gimp.get_pdb().lookup_procedure('file-jpeg-export')
+        save_conf = export_proc.create_config()
         
+        save_conf.set_property('image', image)
+        save_conf.set_property('file', save_file)
+        save_conf.set_property('run-mode', Gimp.RunMode.NONINTERACTIVE)
+        save_conf.set_property('quality', 80) # 80% quality
+        save_conf.set_property('optimize', True)
+        save_conf.set_property('progressive', True)
+        save_conf.set_property('sub-sampling', 0) # 0 is 4:4:4
+        
+        result = export_proc.run(save_conf)
 
-        procedure = Gimp.get_pdb().lookup_procedure('file-jpeg-export'); 
-        config = procedure.create_config(); 
-        config.set_property('run-mode', Gimp.RunMode.INTERACTIVE); 
-        config.set_property('image', image); 
-        config.set_property('file', filename); 
-        #config.set_property('options', options); 
-        config.set_property('quality', 0.8); 
-        config.set_property('smoothing', 0.0000); 
-        config.set_property('optimize', True); 
-        config.set_property('progressive', True); 
-        config.set_property('cmyk', False); 
-        config.set_property('sub-sampling', "sub-sampling-1x1: 4:4:4"); 
-        config.set_property('baseline', True); 
-        config.set_property('restart', 0); 
-        config.set_property('dct', "integer"); 
-        config.set_property('include-exif', False); 
-        config.set_property('include-iptc', True); 
-        config.set_property('include-xmp', False); 
-        config.set_property('include-color-profile', False); 
-        config.set_property('include-thumbnail', False); 
-        config.set_property('include-comment', False); 
-        result = procedure.run(config); success = result.index(0)
-
-        procedure = Gimp.get_pdb().lookup_procedure('gimp-file-save'); 
-        config = procedure.create_config(); 
-        config.set_property('run-mode', Gimp.RunMode.INTERACTIVE); 
-        config.set_property('image', image); 
-        config.set_property('file', filename); 
-        #config.set_property('options', options); 
-        result = procedure.run(config); 
-        success = result.index(0)
-
-
-
-        # do what you want to do, then, in case of success, return:
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
 
+# Important: Add Gio import at the top for the file handling
+from gi.repository import Gio
+
 Gimp.main(ExportWeb.__gtype__, sys.argv)
-      
